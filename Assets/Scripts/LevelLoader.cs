104df8e483c3c147e8ba1d5fca23e60d3715517e
DylanNAron <arondylan72@gmail.com>
2020-11-05 15:12:37 -0500

    AddedUIandRestartButton

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private Scene scene;
+
+    public void Restart()
+    {
+    
+        scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        Time.timeScale = 1;
+    }
+}
--- Assets/Scripts/UIcontrol.cs
+++ Assets/Scripts/UIcontrol.cs
@@ -6,11 +6,39 @@ using UnityEngine.UI;
 public class UIcontrol : MonoBehaviour
 {
     public Text scoreText;
+    public Text timeText;
     private int score = 0;
+    public float time = 5;
+
+    public GameObject gameoverUI;
+    public Text gameoverCurScore;
+    public Text gameoverHighScore;
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
+        timeText.text = time.ToString();
         scoreText.text = score.ToString();
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timeText.text = time.ToString();
+
+        if (time <= 0)
+        {
+            //end game
+            Time.timeScale = 0;
+            timeText.text = 0.ToString();
+            if (score > PlayerPrefs.GetInt("Highscore", 0))
+            {
+                PlayerPrefs.SetInt("Highscore", score);
+            }
+            gameoverCurScore.text = score.ToString();
+            gameoverHighScore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
+            gameoverUI.SetActive(true);
+        }
     }
 
     public void UpdateScore()
